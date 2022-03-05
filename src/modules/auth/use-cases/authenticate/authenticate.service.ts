0f8d5ab75e8a7106910e8ce8d6ca65545c95ab2b
Patrick Mazzuco <patrickmazzuco@gmail.com>
2022-03-05 14:44:36 -0300

    refactor: Move hash util to a provider (#2)

--- src/modules/auth/use-cases/authenticate/authenticate.service.ts
+++ src/modules/auth/use-cases/authenticate/authenticate.service.ts
@@ -1,9 +1,10 @@
 import { AuthErrors } from '@modules/auth/errors/auth.errors';
+import { IPasswordHash } from '@modules/users/providers/password-hash.provider';
 import { IUsersRepository } from '@modules/users/repositories/users-repository.interface';
 import { Inject, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { ProviderToken } from '@src/shared/enums/provider-token.enum';
 import { RepositoryToken } from '@src/shared/enums/repository-token.enum';
-import { PasswordHash } from '@utils/security/password-hash';
 
 import { AuthenticationBodyDTO } from './dtos/authentication-body.dto';
 import { AuthenticationResponseDTO } from './dtos/authentication-response.dto';
@@ -13,6 +14,8 @@ export class AuthenticateService {
   constructor(
     @Inject(RepositoryToken.USERS_REPOSITORY)
     private readonly usersRepository: IUsersRepository,
+    @Inject(ProviderToken.PASSWORD_HASH)
+    private readonly passwordHash: IPasswordHash,
     private readonly jwtService: JwtService,
   ) {}
 
@@ -26,7 +29,10 @@ export class AuthenticateService {
       throw new AuthErrors.InvalidCredentials(this.logger);
     }
 
-    const passwordMatches = await PasswordHash.compare(password, user.password);
+    const passwordMatches = await this.passwordHash.compare(
+      password,
+      user.password,
+    );
 
     if (!passwordMatches) {
       throw new AuthErrors.InvalidCredentials(this.logger);
